2f645adb9c7a150ea4ed143f7a302062fc4354bf
Lakshya <lakshya142000@gmail.com>
2025-07-27 19:49:37 +0530

    Commit-1

---
+++ README.md
@@ -0,0 +1,69 @@
+# Frontend Mentor - Space tourism website solution
+
+This is a solution to the [Space tourism website challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/space-tourism-multipage-website-gRWj1URZ3). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for each of the website's pages depending on their device's screen size
+- See hover states for all interactive elements on the page
+- View each page and be able to toggle between the tabs to see new information
+
+### Screenshot
+#### Home Page
+![](starter-code/assets/screenshots/screencapture-127-0-0-1-5500-starter-code-2025-07-27-19_32_36.png)
+#### Destination Page
+![](starter-code/assets/screenshots/screencapture-127-0-0-1-5500-starter-code-destination-moon-html-2025-07-27-19_33_12.png)
+#### Crew Page
+![](starter-code/assets/screenshots/screencapture-127-0-0-1-5500-starter-code-crew-commander-html-2025-07-27-19_33_30.png)
+#### Technology Page
+![](starter-code/assets/screenshots/screencapture-127-0-0-1-5500-starter-code-technology-vehicle-html-2025-07-27-19_34_02.png)
+#### Tablet View
+![](starter-code/assets/screenshots/screencapture-127-0-0-1-5500-starter-code-index-html-2025-07-27-19_35_25.png)
+#### Mobile View
+![](starter-code/assets/screenshots/screencapture-127-0-0-1-5500-starter-code-index-html-2025-07-27-19_36_10.png)
+#### Mobile View (Sidebar Active)
+![](starter-code/assets/screenshots/screencapture-127-0-0-1-5500-starter-code-index-html-2025-07-27-19_35_51.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+
+### Continued development
+
+I want to focus on making this single page application and not to repeat the code again and again.
+Want to learn more about figma and how to use given dimensions dynamically.
+
+
+## Author
+
+- LinkedIn - [Lakshya Tyagi](https://www.linkedin.com/in/lakshya-tyagi-50341517b/)
+- Frontend Mentor - [@lakshya142000](https://www.frontendmentor.io/profile/lakshya142000)
+- Github -[@lakshya142000](https://github.com/lakshya142000)
+
+
---
+++ starter-code/script.js
@@ -0,0 +1,167 @@
+const home = document.getElementById('home-btn')
+const destination = document.getElementById('destination-btn')
+const crew = document.getElementById('crew-btn')
+const technology = document.getElementById('technology-btn')
+const explore = document.getElementById('circle-button')
+
+home.addEventListener('click',()=>{
+    if(home.classList.contains('active')){
+        home.classList.remove('active')
+    }else{
+        window.location.href = "/starter-code/index.html"
+    }
+})
+destination.addEventListener('click',()=>{
+    if(destination.classList.contains('active')){
+        destination.classList.remove('active')
+    }else{
+        window.location.href = "/starter-code/destination-moon.html"
+    }
+    
+})
+crew.addEventListener('click',()=>{
+    if(crew.classList.contains('active')){
+        crew.classList.remove('active')
+    }else{
+        window.location.href = "/starter-code/crew-commander.html"
+    }
+    
+})
+technology.addEventListener('click',()=>{
+    if(technology.classList.contains('active')){
+        technology.classList.remove('active')
+    }else{
+        window.location.href = "/starter-code/technology-vehicle.html"
+    }
+    
+})
+
+if(explore){
+    explore.addEventListener('click',()=>{
+    window.location.href ='/starter-code/destination-moon.html' 
+})
+}
+
+// handling destination
+
+// const moon = document.getElementById('moon')
+// const mars = document.getElementById('mars')
+// const europa = document.getElementById('europa')
+// const titan = document.getElementById('titan')
+
+// moon.addEventListener('click',()=>{
+//     console.log('moon')
+//     window.location.href ='/starter-code/destination-moon.html' 
+// })
+
+// mars.addEventListener('click',()=>{
+//     window.location.href ='/starter-code/destination-mars.html' 
+// })
+
+// europa.addEventListener('click',()=>{
+//     window.location.href ='/starter-code/destination-europa.html' 
+// })
+
+// titan.addEventListener('click',()=>{
+//     window.location.href ='/starter-code/destination-titan.html' 
+// })
+const sideNav=`<div class="mob-nav-buttons">
+  <svg id="close-btn" xmlns="http://www.w3.org/2000/svg" width="20" height="21" onclick="closeButton()">
+        <g fill="#D0D6F9" fill-rule="evenodd"><path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z"/>
+        <path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z"/></g>
+  </svg>
+  <div id="mob-home-btn" class="mob-nav-container">
+    <span style="font-weight: bold;">00</span>
+    <a class="mob-direction-a">HOME</a>
+  </div>
+  <div id="mob-destination-btn" class="mob-nav-container">
+    <span style="font-weight: bold;">01</span>
+    <a class="mob-direction-a">DESTINATION</a>
+  </div>
+  <div id="mob-crew-btn" class="mob-nav-container">
+    <span style="font-weight: bold;">02</span>
+    <a class="mob-direction-a">CREW</a>
+  </div>
+  <div id="mob-technology-btn" class="mob-nav-container">
+    <span style="font-weight: bold;">03</span>
+    <a class="mob-direction-a">TECHNOLOGY</a>
+  </div>
+  
+</div>`
+
+const closeBtn = document.getElementById('close-btn')
+
+// const mobHome = document.getElementById('mob-home-btn')
+// const mobDestination = document.getElementById('mob-destination-btn')
+// const mobTechnology = document.getElementById('mob-technology-btn')
+// const mobCrew = document.getElementById('mob-crew-btn')
+const hamburger = document.getElementById('hamburger-btn')
+
+
+
+if(hamburger){
+    hamburger.addEventListener('click',()=>{
+    const div = document.createElement('div');
+    div.innerHTML = sideNav;
+    document.body.append(div)
+    hamburger.style.display = 'none'
+    activeLink()
+})
+}
+function activeLink(){
+    const mobHome = document.getElementById('mob-home-btn')
+    const mobDestination = document.getElementById('mob-destination-btn')
+    const mobTechnology = document.getElementById('mob-technology-btn')
+    const mobCrew = document.getElementById('mob-crew-btn')
+   
+    const address = window.location.href
+    if(address.includes('index')){
+        mobHome.classList.add(' ')
+    }
+    else if(address.includes('destination')){
+        mobDestination.classList.add('active')
+    }
+    else if(address.includes('crew')){
+        mobCrew.classList.add('active')
+    }
+    else{
+        mobTechnology.classList.add('active')
+    }
+    mobHome.addEventListener('click',()=>{
+        if(home.classList.contains('active')){
+            home.classList.remove('active')
+        }else{
+            window.location.href = "/starter-code/index.html"
+        }
+    })
+    mobDestination.addEventListener('click',()=>{
+        if(destination.classList.contains('active')){
+            destination.classList.remove('active')
+        }else{
+            window.location.href = "/starter-code/destination-moon.html"
+        }
+        
+    })
+    mobCrew.addEventListener('click',()=>{
+        if(crew.classList.contains('active')){
+            crew.classList.remove('active')
+        }else{
+            window.location.href = "/starter-code/crew-commander.html"
+        }
+        
+    })
+    mobTechnology.addEventListener('click',()=>{
+        if(technology.classList.contains('active')){
+            technology.classList.remove('active')
+        }else{
+            window.location.href = "/starter-code/technology-vehicle.html"
+        }
+        
+    })
+
+}
+function closeButton(){
+    const collection = document.getElementsByClassName('mob-nav-buttons')
+    collection[0].remove()
+    hamburger.style.display = 'flex'
+}
